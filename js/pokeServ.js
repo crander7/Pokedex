0e77efcff50284309268cdaf3b63af557b09622c
crander7 <andersen.craigm@gmail.com>
2016-07-15 17:51:43 -0600

    animation working

--- js/pokeServ.js
+++ js/pokeServ.js
@@ -1,13 +1,19 @@
 angular.module('pokedex')
   .service('pokeServ', function ($http) {
-    console.log('service');
-    this.getPokemon = function (num) {
-      return $http({
-        method: 'GET',
-        url: 'http://pokeapi.co/api/v2/pokemon/' + num + '/'
-      }).then(function (response) {
-        return response.data
-      })
+
+    this.getPokemon = function (searchTerm) {
+      if (isNaN(searchTerm)) {
+        searchTerm = searchTerm.toLowerCase();
+      }
+        return $http({
+          method: 'GET',
+          url: 'http://pokeapi.co/api/v2/pokemon/' + searchTerm + '/'
+        }).then(function (response) {
+          console.log(response);
+          return response.data;
+        }, function () {
+          return "";
+        });
     }
 
 
